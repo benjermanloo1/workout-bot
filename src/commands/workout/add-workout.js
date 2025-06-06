f2e52f93625892e951917440d1e812a2722c5181
Ben Lee <benjamin.s.lee13@gmail.com>
2025-06-06 19:59:04 -0400

    GO GO GO
    Next:
    * Continue working on /add-workout -> connect to database
    * Add a /delete-workout
    * Look into ways to implement /edit-workout

--- src/commands/workout/add-workout.js
+++ src/commands/workout/add-workout.js
@@ -7,18 +7,19 @@ Users can add their workouts.
 
 async function setupModal(interaction) {
     const modal = new ModalBuilder()
-        .setCustomId(interaction.id)
+        .setCustomId('add-workout-modal')
         .setTitle('Add a Workout');
 
     const workoutNameInput = new TextInputBuilder()
         .setCustomId('workoutNameInput')
         .setLabel('Name of workout')
+        .setPlaceholder('e.g. Pull')
         .setStyle(TextInputStyle.Short);
     
     const exerciseInput = new TextInputBuilder()
         .setCustomId('exerciseInput')
         .setLabel('Exercises performed')
-        .setPlaceholder('Name of exercise - # of sets - # of reps')
+        .setPlaceholder('Name of exercise - # of sets - # of reps\ne.g. bench - 3 - 5')
         .setStyle(TextInputStyle.Paragraph);
 
     const nameRow = new ActionRowBuilder().addComponents(workoutNameInput);
@@ -27,7 +28,6 @@ async function setupModal(interaction) {
     modal.addComponents(nameRow, exerciseRow);
 
     await interaction.showModal(modal);
-    await interaction.reply(`${interaction.fields.getTextInputValue('workoutNameInput')}\n${interaction.fields.getTextInputValue('exerciseInput')}`);
 };
 
 
@@ -41,6 +41,19 @@ module.exports = {
         const discordId = interaction.user.id;
         const username = interaction.user.username;
 
-        await setupModal(discordId, username, interaction);
+        await setupModal(interaction);
+
+        const filter = (interaction) => interaction.customId === 'add-workout-modal';
+
+        interaction.awaitModalSubmit({ filter, time: 120_000 })
+            .then((modalInteraction) => {
+                const workoutName = modalInteraction.fields.getTextInputValue('workoutNameInput');
+                const exercises = modalInteraction.fields.getTextInputValue('exerciseInput');
+
+                modalInteraction.reply(`${workoutName}\n${exercises}`);
+            })
+            .catch((error) => {
+                console.log(`${error}`);
+            });
     },
 };
