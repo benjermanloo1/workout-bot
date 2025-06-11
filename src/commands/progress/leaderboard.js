eed1f62a542e6d30096e0537649a377bd43a8a00
Ben Lee <benjamin.s.lee13@gmail.com>
2025-06-11 17:35:13 -0400

    /leaderboard finished
    WOOOOOOOOOOOOOO
    
    i can smell the finish line
    
    Next:
    * /upload and AWS NOOOOOOOOOOOO

--- src/commands/progress/leaderboard.js
+++ src/commands/progress/leaderboard.js
@@ -1,19 +1,98 @@
-const { MessageFlags, SlashCommandBuilder } = require('discord.js');
+const { Lift, User } = require('../../models')
+const { MessageFlags, SlashCommandBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, Message } = require('discord.js');
 
 /*
 Leaderboard:
 Users can see who has the highest singular max lift / combined max lifts.
 */
 
+function capitalizeFirstLetter(str) {
+    if (!str) return str; // handle empty string or null
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
+
+async function getLeaderboard(users, selection) {
+    const lifts = await getLifts(users);
+
+    const sorted = lifts.sort((a, b) => b[selection] - a[selection]);
+
+    const medals = ['🥇', '🥈', '🥉'];
+
+    const leaderboard = sorted.map((entry, index) => {
+        const medal = medals[index] ?? `#${index + 1}`;
+        return `${medal} ${entry.username}: ${entry[selection]} lbs`;
+    });
+
+    return leaderboard.join('\n');
+};
+
+async function getLifts(users) {
+    const lifts = users.map(async ({ discordId, username}) => {
+        try {
+            const [bench, squat, deadlift] = await Promise.all([
+                Lift.findOne({ where: { discordId, liftName: 'bench' } }),
+                Lift.findOne({ where: { discordId, liftName: 'squat' } }),
+                Lift.findOne({ where: { discordId, liftName: 'deadlift' } }),
+            ]);
+
+            const benchWeight = bench?.weight ?? 0;
+            const squatWeight = squat?.weight ?? 0;
+            const deadliftWeight = deadlift?.weight ?? 0;
+
+            return {
+                username,
+                bench: benchWeight,
+                squat: squatWeight,
+                deadlift: deadliftWeight,
+                total: benchWeight + squatWeight + deadliftWeight
+            };
+        } catch (error) {
+            console.log(`Error fetching lifts for ${username}.`);
+            return {
+                username,
+                bench: 0,
+                squat: 0,
+                deadlift: 0,
+                total: 0,
+            };
+        }
+    });
+
+    return await Promise.all(lifts);
+};
 
 module.exports = {
     cooldown: 3,
     category: 'progress',
     data: new SlashCommandBuilder()
         .setName('leaderboard')
-        .setDescription('View the leaderboard.'),
+        .setDescription('View the leaderboard.')
+        .addStringOption(option => 
+            option
+                .setName('rankings')
+                .setDescription('Leaderboard to view.')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'bench press', value: 'bench' },
+                    { name: 'squat', value: 'squat' },
+                    { name: 'deadlift', value: 'deadlift' },
+                    { name: 'overall', value: 'overall' },
+                ),
+            ),
     async execute(interaction) {
-        await interaction.reply('LEADERBOARD FLFLAFLAFLFLF');
+        const users = await User.findAll();
+
+        if (users.length === 0) {
+            await interaction.reply({
+                content: 'There are no users in the database.',
+                flags: MessageFlags.Ephemeral,
+            });
+        } else {
+            const selection = interaction.options.getString('rankings');
+            const leaderboard = await getLeaderboard(users, selection);
+
+            await interaction.reply(`**${capitalizeFirstLetter(selection)} leaderboard:**\n${leaderboard}`);
+        };
     },
 };
